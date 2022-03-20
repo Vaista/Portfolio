e984bedebe993e9827252252e3afcfbc98da3949
Vaibhav <vsharma5295@gmail.com>
2022-03-20 16:53:40 +0530

    initializing portfolio

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
+from smtplib import SMTP
+
+
+app = Flask(__name__)
+smtp_email = 'aforalfabforbravo12@outlook.com'
+receiving_email = 'vsharma5295@gmail.com'
+smtp_pwd = '1PF(aO6GiTP8l4LAt#'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    date = datetime.now().year
+    if request.method == 'POST':
+        name = request.form.get('username')
+        email = request.form.get('email')
+        message = request.form.get('msg')
+        try:
+            connection = SMTP("smtp.office365.com", port=587)
+            connection.starttls()
+            connection.login(smtp_email, smtp_pwd)
+            connection.sendmail(from_addr=smtp_email,
+                                to_addrs=receiving_email,
+                                msg=f"Subject:Message received from website\n\n{message}\nSender Name: {name}\nSender Email: {email}")
+            connection.close()
+        except:
+            pass
+        return redirect(url_for('home'))
+    return render_template('index.html', year=date)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
